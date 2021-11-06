aa83fe61079ffc1270173fcd6d18b9251e5dea55
harimuhlia <harimuhlia@gmail.com>
2021-11-06 20:09:15 +0700

    Comment Link Youtube Di Frontend Blade

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -6,7 +6,7 @@
     <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
       <h1 class="mb-4 pb-0">Selamat Datang<br><span>Di</span> Ektrakurikuler</h1>
       <p class="mb-4 pb-0">SMK Negeri 5 Kabupaten Tangerang Banten</p>
-      <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox play-btn mb-4"></a>
+      {{-- <a href="https://youtu.be/7nOOV_MrpxI" class="glightbox play-btn mb-4"></a> --}}
       <a href="#about" class="about-btn scrollto">Tentang MenskullApp</a>
     </div>
   </section><!-- End Hero Section -->
